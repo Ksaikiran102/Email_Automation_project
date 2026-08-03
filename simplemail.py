95e669a68f46f22edbaa89132cce8be2c6a7e505
SaiKiran Kasarla <kasarlasaikiran002@gmail.com>
2026-08-04 02:10:41 +0530

    Update simplemail.py

--- simplemail.py
+++ simplemail.py
@@ -3,10 +3,12 @@ Step 1:-->Setting up Gmail App Password(2 step verification)
 #sxlk uqbw yvgm zcqe
 We will use SMTP (simple Mail Transfer Protocol)
 Step 2:using SMTPLIB we start the communication
+
 '''
 import smtplib
 #first we will make the protocol connection
 From=input("Enter mail Address:")
+To=input('Enter To Email address:')
 pas=input("Enter 12 letter password")
 server = smtplib.SMTP('smtp.gmail.com',587)
 print(server)
@@ -17,5 +19,5 @@ server.login(From,pas)
 print("Login Success")
 messsage="Hello,This is an Automated Mail"
 #send the mail
-server.sendmail("kasarlasaikiran002@gamil.com","22311a05n3@cse.sreenidhi.edu.in",messsage)
+server.sendmail(From,To,messsage)
 print("Success")
